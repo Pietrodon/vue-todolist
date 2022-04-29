aa6b797f979c810404ae7d7d329023580383372e
Pietrodon <donvitopietro00@gmail.com>
2022-04-29 17:26:58 +0200

    aggiunta input

--- app.js
+++ app.js
@@ -6,5 +6,15 @@ const app = new Vue({
             'bere acqua',
             'dici ciao'
         ],
+        newtodo:"",
     },
-})
+    methods:{
+        add: function(){
+            this.todo.push(this.newtodo)
+        },
+        remove:function(){
+            console.log('eliminare il todo')
+        }
+    }
+
+});
